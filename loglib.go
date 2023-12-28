e31a233ce4a0b72a342f36cc81ec66be29e8cb29
andjyy <bjekovic.andjela@gmail.com>
2023-12-28 11:06:02 +0100

    initial commit

--- loglib.go
+++ loglib.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler {
+func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
 		log.Info(r.Method, r.URL, r.Host)
