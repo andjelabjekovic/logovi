4f726f9022d86d74cc1068245b2ae9c9d53241a5
andjyy <bjekovic.andjela@gmail.com>
2024-01-18 20:44:24 +0100

    edit

--- loglib.go
+++ loglib.go
@@ -7,13 +7,13 @@ import (
 	"net/http"
 )
 
-func LogInit(path string) (*log.Logger, mux.MiddlewareFunc) {
+func LogInit(path string) (*log.Logger, mux.MiddlewareFunc, func(r *http.Request, msg string), func(r *http.Request, msg string), func(r *http.Request, msg string)) {
 	logger := log.New()
 	logger.SetFormatter(&log.JSONFormatter{})
 
 	logger.Out = &lumberjack.Logger{
 		Filename:   path,
-		MaxSize:    500, // megabytes
+		MaxSize:    500, // megabytesloglib
 		MaxBackups: 3,
 		MaxAge:     28,   //days
 		Compress:   true, // disabled by default
@@ -25,19 +25,41 @@ func LogInit(path string) (*log.Logger, mux.MiddlewareFunc) {
 		log.Error("Ne mogu postaviti fajl za čuvanje logova. Koristiću osnovni izlaz.")
 	}*/
 
+	writeFatal := func(r *http.Request, msg string) {
+		logger.WithFields(log.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"ip":     r.Host,
+		}).Fatal(msg)
+	}
+	writeInfo := func(r *http.Request, msg string) {
+		logger.WithFields(log.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"ip":     r.Host,
+		}).Info(msg)
+	}
+	writeWarning := func(r *http.Request, msg string) {
+		logger.WithFields(log.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"ip":     r.Host,
+		}).Warning(msg)
+	}
+
 	loggingMiddleware := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Do stuff here
 			logger.WithFields(log.Fields{
 				"method": r.Method,
-				"url":    r.URL,
-				"ip":     r.Host,
-			}).Info("Failed to send event")
+				"url":    r.RequestURI,
+				"ip":     r.RemoteAddr,
+			}).Info("Request expected")
 			//log.Info(r.Method, r.URL, r.Host)
 
 			// Call the next handler, which can be another middleware in the chain, or the final handler.
 			next.ServeHTTP(w, r)
 		})
 	}
-	return logger, loggingMiddleware
+	return logger, loggingMiddleware, writeFatal, writeInfo, writeWarning
 }
